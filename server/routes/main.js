161bfe794cd5f1c551a9e1f3475db32b5851ba92
J.Lyu <jlyu@wpi.edu>
2018-04-29 18:40:12 -0400

    [Server] Add /products and /product/:id routers to main.js.

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,6 +5,43 @@ const async = require('async');	// product loading pagination
 const Category = require('../models/category');
 const Product = require('../models/product');
 
+// get all products
+router.get('/products', (req, res, next) => {
+	const perPage = 10;
+	const page = req.query.page;
+	async.parallel([
+		function(callback) {
+			Product.count({}, (err, count) => {
+				let totalProducts = count;
+				callback(err, totalProducts);
+			});
+		},
+		function(callback) {
+			Product.find({})
+				.skip(perPage * page)
+				.limit(perPage)
+				.populate('category')
+				.populate('owner')
+				.exec((err, products) => {
+					if (err) { return next(err); }
+					callback(err, products);
+				});
+		}
+	], function(err, results) {
+		let totalProducts = results[0];
+		let products = results[1];
+
+		res.json({
+			success: true,
+			message: 'category',
+			products: products,
+			totalProducts: totalProducts,
+			pages: Math.ceil(totalProducts / perPage)
+		});
+	});
+});
+
+
 router.route('/categories')
 	.get((req, res, next) => {
 		Category.find({}, (err, categories) => {
@@ -51,7 +88,7 @@ router.get('/categories/:id', (req, res, next) => {
 		function(callback) {
 			Product.count({ category: req.params.id }, (err, count) => {
 				let totalProducts = count;
-				callback(err, totalProducts);	// trigger the next function
+				callback(err, totalProducts);
 			});
 		},
 		// find all products under the given category
@@ -88,5 +125,28 @@ router.get('/categories/:id', (req, res, next) => {
 		});
 	});
 });
+
+// get a single product
+router.get('/product/:id', (req, res, next) => {
+	Product.findById({ _id: req.params.id })
+		.populate('category')
+		.populate('owner')
+		.exec((err, product) => {
+			if (err) {
+				res.json({
+					success: false,
+					message: 'product not found'
+				});
+			} else {
+				if (product) {
+					res.json({
+						success: true,
+						product: product
+					});
+				}
+			}
+		});
+});
+
 
 module.exports = router;
