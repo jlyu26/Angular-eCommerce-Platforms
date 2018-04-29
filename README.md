34c6cc9b572c6418b8e3cbfb18abbd719609d97b
J.Lyu <jlyu@wpi.edu>
2018-04-29 15:47:33 -0400

    [Server] Not best practice: get product(s) belong to a specific category

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Angular-eCommerce-Platforms
 
 An eCommerce website based on Angular 5, Node.js, MongoDB and AWS S3.
+
+## Notes:
+
+If the URL is `/api/categories/:id`, to get the specific id, we have to make request `req.params.id`. Let's say `id = 'awesome'`, where we write `req.params.id` equals to `'awesome'`.
+
+The other way to get value from URL is `req.query`. For example, if the URL is `/api/categories/name?anyVariable=anyValue`, in order to get `'anyValue'`, we need to make `req.query.anyVariable`. This is usually used in search of a product or so in search engine.
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,7 @@
 
 const router = require('express').Router();
 const Category = require('../models/category');
+const Product = require('../models/product');
 
 router.route('/categories')
 	.get((req, res, next) => {
@@ -22,5 +23,25 @@ router.route('/categories')
 			message: 'successful'
 		});
 	});
+
+// Not the best practice: What if we got 10,000 products?
+// Shouldn't send all of them to front-end at once.
+router.get('/categories/:id', (req, res, next) => {
+	const perPage = 10;
+	Product.find({ category: req.params.id })	// find all products belong to the specific category
+		.populate('category')
+		.exec((err, products) => {
+			Product.count({ category: req.params.id }, (err, totalProducts) => {
+				res.json({
+					success: true,
+					message: 'category',
+					products: products,
+					categoryName: products[0].category.name,
+					totalProducts: totalProducts,
+					pages: Math.ceil(totalProducts / perPage)
+				});
+			});
+		});
+});
 
 module.exports = router;
